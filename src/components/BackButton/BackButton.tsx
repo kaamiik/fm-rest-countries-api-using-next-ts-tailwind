eee2ec6fa3eb7b2c41ebe67283244184809cf5f0
Kamran Kiani <kiani.km@gmail.com>
2025-07-24 12:15:35 +0330

    Create BackButton.tsx

---
+++ src/components/BackButton/BackButton.tsx
@@ -0,0 +1,27 @@
+'use client';
+import * as React from 'react';
+import { useRouter } from 'next/navigation';
+
+import Icon from '../Icon';
+
+import { useTheme } from '@/components/ThemeProvider';
+
+function BackButton() {
+  const router = useRouter();
+  const { theme } = useTheme();
+  return (
+    <button
+      className="dark:bg-dark-blue md:text-400 shadow-focus-dark flex w-fit cursor-pointer items-center gap-2 rounded-sm bg-white px-6 py-2 md:rounded-md md:px-10"
+      onClick={() => router.back()}
+    >
+      <Icon
+        src={theme === 'light' ? '/back-arrow.svg' : '/back-arrow-dark.svg'}
+        width={20}
+        height={20}
+      />
+      Back
+    </button>
+  );
+}
+
+export default BackButton;
