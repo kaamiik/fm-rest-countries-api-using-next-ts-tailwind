75673822760ce78362fce5ee9570450b1a66168a
Kamran Kiani <kiani.km@gmail.com>
2025-07-24 12:15:14 +0330

    Refactor Home page with components and suspense
    Replaces the Home page's empty div with a structured layout using MaxWidthWrapper, SearchForm, CountryList, and Spinner. Implements React.Suspense for lazy loading CountryList and improves accessibility with a hidden heading.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div></div>;
+import React from 'react';
+
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import SearchForm from '@/components/SearchForm';
+import CountryList from '@/components/CountryList';
+import Spinner from '@/components/Spinner';
+
+export default async function Home() {
+  return (
+    <MaxWidthWrapper
+      as="main"
+      className="bg-light-gray dark:bg-darker-blue min-h-dvh px-4 pt-6 pb-16 md:py-12 xl:px-0"
+    >
+      <h1 className="sr-only">Rest Countries API</h1>
+      <SearchForm />
+
+      <React.Suspense fallback={<Spinner />}>
+        <CountryList />
+      </React.Suspense>
+    </MaxWidthWrapper>
+  );
 }
