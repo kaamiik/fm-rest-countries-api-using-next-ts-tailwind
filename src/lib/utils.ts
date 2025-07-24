9b7923b81a698a719c0d478d720955b5925bfc69
Kamran Kiani <kiani.km@gmail.com>
2025-07-24 12:17:14 +0330

    Create utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,32 @@
+import type { Country } from '@/types';
+
+export async function getData() {
+  const response = await fetch(
+    'https://restcountries.com/v3.1/all?fields=name,flags,cca2,cca3,borders,capital,currencies,population,region,tld'
+  );
+  const data = await response.json();
+
+  return data;
+}
+
+export function getFilteredData(
+  data: Country[],
+  searchParams: { search?: string; region?: string } = {}
+) {
+  const filteredData = data.filter((country) => {
+    const searchTerm = searchParams.search?.toLowerCase().trim() || '';
+    const matchesSearch = searchTerm
+      ? country.name.common.toLowerCase().includes(searchTerm) ||
+        country.cca2.toLowerCase().includes(searchTerm) ||
+        country.cca3.toLowerCase().includes(searchTerm)
+      : true;
+
+    const matchesRegion = searchParams.region
+      ? country.region === searchParams.region.trim()
+      : true;
+
+    return matchesSearch && matchesRegion;
+  });
+
+  return filteredData;
+}
