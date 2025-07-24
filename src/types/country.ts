7417f3eeeab505f2a2d44a29df2b6c96b906c3ff
Kamran Kiani <kiani.km@gmail.com>
2025-07-24 12:17:18 +0330

    Create country.ts

---
+++ src/types/country.ts
@@ -0,0 +1,33 @@
+export type Currency = {
+  name: string;
+  symbol?: string;
+};
+
+export type Country = {
+  flags: {
+    png: string;
+    svg: string;
+    alt: string;
+  };
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      [languageCode: string]: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  tld: string[];
+  cca2: string;
+  cca3: string;
+  currencies: {
+    [currencyCode: string]: Currency;
+  };
+  capital: string[];
+  region: string;
+  // subregion: string;
+  borders: string[];
+  population: number;
+};
